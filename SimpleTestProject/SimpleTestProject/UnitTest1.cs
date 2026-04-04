d2131c5a6f0e7c3fa1d3ad9493675f229c505371
Julie <lam.julie@hotmail.com>
2026-04-04 12:53:21 +1100

    Test with a fail test

--- SimpleTestProject/SimpleTestProject/UnitTest1.cs
+++ SimpleTestProject/SimpleTestProject/UnitTest1.cs
@@ -10,7 +10,9 @@ namespace SimpleTestProject
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Assert.Pass();
+
+            Assert.Fail(); 
         }
     }
 }
